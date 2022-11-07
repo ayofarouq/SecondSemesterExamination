6b5dfe42466d406eaffff0c7ba5c324a38fd4122
Farouq Olanrewaju <olanrewajufarouq@gmail.com>
2022-11-07 14:59:40 +0100

    Just some minor changes

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useContext } from "react";
+import CheckAuth from "../middleware/auth";
+import NavigationContainer from "./Navigation";
+import { UserContext } from "../context/UserContext";
+
+export default function HomeContainer() {
+  const { user } = useContext(UserContext);
+  return (
+    <>
+      <div>
+        <NavigationContainer />
+      </div>
+      <div className="capitalize container">
+        <div>Logged in user: {user?.name}</div>
+        <div>Time logged in: {JSON.parse(JSON.stringify(user?.time || ""))}</div>
+      </div>n
+    </>
+  );
+}
---
+++ src/middleware/auth.js
@@ -0,0 +1,6 @@
+export default function CheckAuth(user) {
+    if (user?.name && user?.password) {
+      return { status: true, user };
+    } else return { status: false, user: null };
+  }
+  
